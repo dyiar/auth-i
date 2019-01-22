1d3e15c4dbf872ea9d27a10ebbca4a37dca9a35c
dyiar <austin.hl.blake@gmail.com>
2019-01-22 14:56:35 -0700

    finished MVP and 1 part of stretch.

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
 const express = require('express')
+const helmet = require('helmet');
 const cors = require('cors')
 const bcrypt= require('bcrypt')
+const session = require('express-session')
+const KnexSessionsStore = require('connect-session-knex')(session)
 
 const knex = require('knex')
 const knexConfig = require('./knexfile')
 const db = knex(knexConfig.development);
 
 const server = express();
+const sessionConfig = {
+    name: 'austin',
+    secret: 'nas;lkjsienaslcmmas;lkjfeiscnna;12349icm;alksdf',
+    cookie: {
+        maxAge: 1000 * 60,
+        secure: false,
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionsStore({
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        knex: db,
+        createtable: true,
+        clearInterval: 1000 * 60 * 10
+    })
+}
 
 server.use(express.json());
 server.use(cors());
+server.use(helmet());
+server.use(session(sessionConfig))
+
+//middleware
+
+function protected(req, res, next) {
+    //if the user is logged in NEXT()
+    if (req.session && req.session.user) {
+        next()
+    } else {
+        res.status(401).json({ message: 'you shall not pass, not authenticated.' })
+    }
+}
+
+//endpoints
+
 
 server.get('/', (req,res) => {
     res.send('its alive')
 })
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
     db('users')
-    .select('id', 'username', 'password')
+    .select('id', 'username')
     .then(users => {
         res.json(users)
     })
@@ -44,12 +81,27 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
         if (user && bcrypt.compareSync(creds.password, user.password)) {
-            res.status(200).json({ message: 'welcome' })
+            req.session.user = user;
+            res.status(200).json({ message: `Welcome ${user.username}` })
         } else {
             res.status(401).json({ message: 'you shall not pass' })
         }
     })
     .catch(() => res.status(500).send( 'error' ))
+})
+
+server.get('/api/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if(err) {
+                res.status(500).send('error, cant logout')
+            } else {
+                res.status(200).send('bon voyage')
+            }
+        })
+    } else {
+        res.json({ message: 'already logged out' })
+    }
 })
 
 server.listen(3000, () => console.log('server running'))
